b0976ea47e413cf48ccbf1bdd5964b5b8b688d1f
Joshua Banga <joshuabanga@hotmail.com>
2025-06-04 16:16:10 +0100

    player sprite movement

--- player.py
+++ player.py
@@ -27,6 +27,14 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(dt)
-        if keys[pygame.K_d]:
             self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward_vector = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward_vector * (PLAYER_SPEED * dt)
